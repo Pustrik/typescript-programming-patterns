27fd042628c906585a4150314b5ce8ca66435a6e
Dmytro Pustovalov <dmytro.pustovalov@itomy.ch>
2023-04-06 16:55:51 +0300

    pattern - factory

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import {Singleton} from "./singleton";
+import {FactoryExample} from "./factory";
 
 async function bootstrap() {
         // Singleton programming pattern
-        console.log(`Singleton template: `);
+        console.log(`Singleton pattern: `);
         const singletonInstance1 = Singleton.getInstance()
         const singletonInstance2 = Singleton.getInstance()
         if(singletonInstance1 === singletonInstance2)
@@ -13,6 +14,16 @@ async function bootstrap() {
         Singleton.setPayload(8080);
         const singletonPayload = Singleton.getPayload();
         console.log(`Singleton payload: ` + singletonPayload);
+        console.log();
+
+        // Factory programming pattern
+        console.log(`Factory pattern: `);
+        const factory = new FactoryExample;
+        const product1 = factory.creator(1);
+        const product2 = factory.creator(2);
+        console.log(`First product: ` + product1.assemble());
+        console.log(`Second product: ` + product2.assemble());
+        console.log();
 
 
 }
